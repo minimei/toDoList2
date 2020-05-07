626e5b175025c04460cfa917bb6b09b2ab8cdf49
meimei <5474@ait.nsw.edu.au>
2020-05-08 00:21:00 +1000

    Update

--- platforms/browser/www/js/index2.js
+++ platforms/browser/www/js/index2.js
@@ -1,60 +1,55 @@
-$( document ).on( "pageinit", "#demo-page", function() {
-    // Swipe to remove list item
-    $( document ).on( "swipeleft swiperight", "#list li.ui-li", function( event ) {
-        var listitem = $( this ),
-            // These are the classnames used for the CSS transition
-            dir = event.type === "swipeleft" ? "left" : "right",
-            // Check if the browser supports the transform (3D) CSS transition
-            transition = $.support.cssTransform3d ? dir : false;
-            confirmAndDelete( listitem, transition );
-    });
-    // If it's not a touch device...
-    if ( ! $.mobile.support.touch ) {
-        // Remove the class that is used to hide the delete button on touch devices
-        $( "#list" ).removeClass( "touch" );
-        // Click delete split-button to remove list item
-        $( ".delete" ).on( "click", function() {
-            var listitem = $( this ).parent( "li.ui-li" );
-            confirmAndDelete( listitem );
-        });
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
-    function confirmAndDelete( listitem, transition ) {
-        // Highlight the list item that will be removed
-        listitem.addClass( "ui-btn-down-d" );
-        // Inject topic in confirmation popup after removing any previous injected topics
-        $( "#confirm .topic" ).remove();
-        listitem.find( ".topic" ).clone().insertAfter( "#question" );
-        // Show the confirmation popup
-        $( "#confirm" ).popup( "open" );
-        // Proceed when the user confirms
-        $( "#confirm #yes" ).on( "click", function() {
-            // Remove with a transition
-            if ( transition ) {
-                listitem
-                    // Remove the highlight
-                    .removeClass( "ui-btn-down-d" )
-                    // Add the class for the transition direction
-                    .addClass( transition )
-                    // When the transition is done...
-                    .on( "webkitTransitionEnd transitionend otransitionend", function() {
-                        // ...the list item will be removed
-                        listitem.remove();
-                        // ...the list will be refreshed and the temporary class for border styling removed
-                        $( "#list" ).listview( "refresh" ).find( ".ui-li.border" ).removeClass( "border" );
-                    })
-                    // During the transition the previous list item should get bottom border
-                    .prev( "li.ui-li" ).addClass( "border" );
-            }
-            // If it's not a touch device or the CSS transition isn't supported just remove the list item and refresh the list
-            else {
-                listitem.remove();
-                $( "#list" ).listview( "refresh" );
-            }
-        });
-        // Remove active state and unbind when the cancel button is clicked
-        $( "#confirm #cancel" ).on( "click", function() {
-            listitem.removeClass( "ui-btn-down-d" );
-            $( "#confirm #yes" ).off();
-        });
-    }
-});
+  }
+}
